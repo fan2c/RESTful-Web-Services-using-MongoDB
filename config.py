616883f3b9caebcdf4da56ddc0730b55b21b83b6
fan2c <pxf1017@hotmail.com>
2016-08-01 23:20:01 +0900

    fix

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
     SECRET_KEY = 'fantc0000000987234'
 
--- manage.py
+++ manage.py
@@ -1,9 +1,7 @@
 import os
-#from app import create_app, db
 from app import create_app, mongo
 from app.models import *
 from flask_script import Manager, Shell
-#from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
